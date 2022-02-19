76f86dadc054affd11ff6becceb25bad7fe0c6e1
aanoorzaie <aanoorzaie@gmail.com>
2022-02-19 15:26:51 +0330

    Initialize project fonts, theme and packages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import '../styles/globals.css'
+import theme from '../src/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,13 +11,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <title>To DO App</title>
+
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
 
         <link rel="manifest" href="/manifest.json" />
         <link
@@ -32,7 +45,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
